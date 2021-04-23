76bd41a1ab2f08188384c466496780b081943e42
jeffrey rios jr <jeffriosjr@yahoo.com>
2021-04-23 13:57:43 -0700

    feature(core-components): adding components and pages fro the app architecture

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,11 @@
+const Navbar = () => {
+    return <div id='navbar'>
+        <div className="row text-center">
+            <div className="col">
+                <h2>Navbar Page!</h2>
+            </div>
+        </div>
+    </div>
+}
+
+export default Navbar;
