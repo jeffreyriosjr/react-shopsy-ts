8fc796738a8ab65d0c22a5755995353a7b94e804
jeffrey rios jr <jeffriosjr@yahoo.com>
2021-04-23 14:27:07 -0700

    feature(app-router): passing Router in app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,12 @@
-import React from 'react';
-import logo from './logo.svg';
+import {BrowserRouter as Router} from 'react-router-dom';
+import AppRoutes from './routers/AppRoutes';
 import './App.css';
 
 const App =() => {
   return (
-    <div className="container text-center">
-      <div className="row">
-        <div className="col">
-          <h2>This is my App!</h2>
-        </div>
-      </div>
-    </div>
+    <Router>
+      <AppRoutes />
+    </Router>
   );
 }
 
