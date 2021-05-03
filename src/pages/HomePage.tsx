ae9857df7f46cc1a3f2628a1f5d8651a05a63b9f
jeffrey rios jr <jeffriosjr@yahoo.com>
2021-05-03 13:40:47 -0700

    feat(home-page): styling a product home card for e/ merch item

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,6 +1,7 @@
 import { useEffect, useContext } from 'react';
 import {GlobalContext} from '../context/GlobalContext';
-
+import {Link} from 'react-router-dom';
+import '../App.css';
 const HomePage = () => {
   const { products, getProducts} = useContext(
     GlobalContext
@@ -11,6 +12,7 @@ const HomePage = () => {
   }, []);
 
   return (
+    <div className='container'>
     <div id='home'>
       <div className='row text-center'>
         <div className='col'>
@@ -19,18 +21,40 @@ const HomePage = () => {
         </div>
       </div>
       <div className="row">
-        <div className="col-md-8 offset-md-2">
-          <ul className="list-group">
+       
             {products.map((product, i) => {
               return (
-                <li className="list-group-item" key={i}>
-                  {product.title}
-                </li>
-              )
+                <div className="col-sm-12 col-md-3 mb-3" key={i}>
+                  <div className="card h-100 d-flex justify-content-around">
+                    {/* img-on-top */}
+                    <div className="sh-card-img">
+                        <div className="sh-bg-img" style={{backgroundImage: `url(${product.image})`}}>
+                        </div>
+                    <div className="card-body">
+                      <h5 className="card-title">
+                        <Link to={`/products/${product.id}`}>{product.title}</Link></h5>
+                      <div className='d-flex justify-content-between align-items-center'>
+                        <strong>${product.price}</strong>
+                        <span className="badge badge-warning">
+                          {product.category}
+                        </span>
+                      </div>
+                    </div>
+
+                    <div className="card-footer">
+                      <button className="btn btn-block btn-primary">
+                        Add to Cart
+                      </button>
+                    </div>
+
+                    </div>
+                  </div>
+                </div>
+              );
             })}
-          </ul>
-        </div>
+        
       </div>
+    </div>
     </div>
   );
 };
